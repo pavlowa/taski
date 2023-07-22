0db64082aac8ed568244e6181320172d41d3bec2
Tatiana Pavlova <bakusha68@yandex.ru>
2023-07-22 14:30:37 +0000

    Completed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,19 @@
+import os
+import sentry_sdk
+from dotenv import load_dotenv
 from pathlib import Path
+from sentry_sdk.integrations.django import DjangoIntegration
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.67.65', '127.0.0.1', 'localhost', 'tasklist.sytes.net']
 
 
 # Application definition
@@ -103,7 +109,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -113,3 +120,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
+
+sentry_sdk.init(
+    dsn=" https://be7fab51e10b4b3db882a7681ede83ca@o4505551014920192.ingest.sentry.io/4505551018393600",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    traces_sample_rate=1.0,
+
+    send_default_pii=True
+)
