573ebf055d90fd45081e5aad8b818b904f0c25c0
Tatiana Pavlova <bakusha68@yandex.ru>
2023-07-25 15:13:19 +0000

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,12 +8,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['158.160.67.65', '127.0.0.1', 'localhost', 'tasklist.sytes.net']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
